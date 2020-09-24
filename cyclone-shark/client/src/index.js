2ee2fe0f614764b9d8935bab3e8364c8e4d91dd6
Kovner Chalumo <kchalumo@gmail.com>
2020-09-24 19:55:12 -0400

    initialized store and implemented productIdSlice

--- cyclone-shark/client/src/index.js
+++ cyclone-shark/client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
 
+
 var mountNode = document.getElementById('app');
 
 ReactDOM.render(<App name='Jane' />, mountNode);
