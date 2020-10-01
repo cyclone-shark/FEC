27a28806844bc543960ba9036edd62e3c6a93796
Kovner Chalumo <kchalumo@gmail.com>
2020-10-01 10:56:25 -0400

    added helpers for ratings and reviews

--- helpers/apiHelpers.js
+++ helpers/apiHelpers.js
@@ -23,9 +23,53 @@ const getProductStyles = (id) => {
       .catch((err) => console.error(err))
   );
 };
+//List Reviews
+const getReviewsForProduct = (productId) => {
+  return axios.get(`http://18.224.37.110/reviews?product_id=${productId}`);
+};
+//Get Review Metadata
+const getReviewMetadataForProduct = (productId) => {
+  return axios.get(`http://18.224.37.110/reviews/meta?product_id=${productId}`);
+};
+/* Add a Review for a Product 
+This API call takes Body Parameters:
+
+| Parameter       | Type   | Description                                                  |
+| --------------- | ------ | ------------------------------------------------------------ |
+| product_id      | integer| Required ID of the product to post the review for            |
+| rating          | int    | Integer (1-5) indicating the review rating                   |
+| summary         | text   | Summary text of the review                                   |
+| body            | text   | Continued or full text of the review                         |
+| recommend       | bool   | Value indicating if the reviewer recommends the product      |
+| name            | text   | Username for question asker                                  |
+| email           | text   | Email address for question asker                             |
+| photos          | [text] | Array of text urls that link to images to be shown           |
+| characteristics | object | Object of keys representing characteristic_id and values representing the review value for that characteristic.
+*/
+const addReview = (reviewObj) => {
+  return axios.post(`http://18.224.37.110/reviews`, reviewObj);
+};
+//Mark Review as Helpful
+const markReviewHelpful = (reviewId) => {
+  return axios.put(`http://18.224.37.110/reviews/${reviewId}/helpful`);
+};
+//Updates a review to show it was reported. 
+const reportReview = (reviewId) =>  {
+  return axios.put(`http://18.224.37.110/reviews/${reviewId}/report`);
+};
+
 
 // const getRelatedProducts = (id) => {
 //   axios.get(`http://18.224.37.110/products/${id}/related`)
 // }
 
-export { getAllProducts, getProduct, getProductStyles };
+export { 
+  getAllProducts, 
+  getProduct, 
+  getProductStyles,
+  getReviewMetadataForProduct,
+  getReviewsForProduct,
+  addReview,
+  markReviewHelpful,
+  reportReview
+ };
