c0b1ac127fbd314edbd2c3b05520b3be20e3a942
Tasnim <tasnimazad17@gmail.com>
2020-10-05 14:03:46 -0400

    dynamically render q&a on the page

--- client/src/components/QuestionsAndAnswers/GetQuestions.jsx
+++ client/src/components/QuestionsAndAnswers/GetQuestions.jsx
@@ -1,46 +1,57 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import ShowAnswers from './ShowAnswers';
-
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import ShowAnswers from "./ShowAnswers";
 
 const GetQuestions = () => {
-
   const [questions, setQuestions] = useState([]);
 
+  const id = useSelector((state) => state.productId);
+  //console.log("this is ", id);
+
   //Fetching questions from the API
-  // useEffect(()=> {
-  //   const getAnsweredQuestion = (id) => {
-  //     axios.get('http://18.224.37.110/qa/questions', {
+  useEffect(() => {
+    const getAnsweredQuestion = (id) => {
+      axios
+        .get("http://18.224.37.110/qa/questions", {
+          params: {
+            product_id: id,
+            page: 1,
+            count: 50,
+          },
+        })
+        .then(({ data }) => setQuestions(data.results))
+        .catch((err) => console.log(err));
+    };
+
+    getAnsweredQuestion(id);
+  }, [id]);
+
+  // useEffect(() => {
+  //   const getAnsweredQuestion = async () => {
+  //     const response = await axios.get("http://18.224.37.110/qa/questions", {
   //       params: {
   //         product_id: id,
-  //         page:1,
-  //         count:50
-  //       }
-  //     }).then(({data}) => setQuestions(data.results)).catch((err) => console.log(err))
-  //   }
-  // })
+  //         page: 1,
+  //         count: 50,
+  //       },
+  //     });
+  //     console.log(response)
+  //     setQuestions(response.data);
+  //   };
+  //   getAnsweredQuestion();
+  // }, [id]);
 
-    const getAnsweredQuestion = (id) => {
-      axios.get('http://18.224.37.110/qa/questions', {
-        params: {
-          product_id: id,
-          page:1,
-          count:50
-        }
-      }).then(({data}) => setQuestions(data.results)).catch((err) => console.log(err))
-    }
-    getAnsweredQuestion(20)
+
+
 
   const [show, setShow] = useState(questions.length > 2 ? true : false);
 
   return (
     <div>
-      <ShowAnswers questions={questions}/>
+      <ShowAnswers questions={questions} />
     </div>
-  )
-
-}
-
+  );
+};
 
 export default GetQuestions;
-
