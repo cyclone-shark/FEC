a1eb627f33faef13a78be858ed97234d82fb4116
Daniel Kang <dkang23@gmail.com>
2020-10-09 14:31:06 -0400

    add to cart handler

--- client/src/components/overview/right_side/AddToCart.jsx
+++ client/src/components/overview/right_side/AddToCart.jsx
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-
+import axios from 'axios';
 const AddToCart = (props) => {
   const sku = useSelector((state) => state.sku);
+
+  var clickHandler = () => {
+    if (sku) {
+      axios.post('http://18.224.37.110/cart', { sku_id: sku });
+    } else {
+      console.log('pick a size!');
+    }
+  };
   return (
     <React.Fragment>
-      <button className='cart' onClick={() => console.log(sku)}>
+      <button className='cart' onClick={() => clickHandler()}>
         Add To Cart
       </button>
     </React.Fragment>
